3e777919fd35aa87855ee1c51dd44599cd9c8f1d
yen0x <gege.dert@gmail.com>
2015-09-06 01:23:31 +0200

    ajout de parking

---
+++ app/components/addparking/addparkingController.js
@@ -0,0 +1,22 @@
+var app = angular.module('parking.controllers');
+
+app.controller('AddPrkgCtrl', ['$scope', '$http', function ($scope,  $http) {
+    $scope.parking = {};
+
+    $scope.submit = function() {
+        console.log($scope.parking);
+        $http.post('/api/parking/create',
+            $scope.parking
+            )
+            .then(function(response) {
+                alert('Parking ajouté.');
+            }, function(response) {
+                // called asynchronously if an error occurs
+                // or server returns response with an error status.
+                switch (response.status) {
+                    default:
+                        alert("Une erreur est survenue, merci d'essayer à nouveau dans quelques instants");
+                }
+            });
+    };
+}]);
