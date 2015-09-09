5bf0d7f0f1e88abf3fbab617e830d1e60d2d93da
yen0x <gege.dert@gmail.com>
2015-09-10 00:35:19 +0200

    addparking: geocoding parking address

--- app/components/addparking/addparkingController.js
+++ app/components/addparking/addparkingController.js
@@ -48,6 +48,26 @@ app.controller('AddPrkgCtrl', ['$scope', '$http', function ($scope, $http) {
             });
     };
 
+    $scope.getAddress = function () {
+        var query = $scope.parking.address + "+" + $scope.parking.zip + "+" + $scope.parking.city;
+        $http.get("https://api.mapbox.com/v4/geocode/mapbox.places/" +
+            query.replace(" ", "+").replace("undefined", "").trim() +
+            ".json?access_token=pk.eyJ1IjoieWVub3giLCJhIjoiODQyNmI4YjA5NjI4MjY0MTYwYzI3NDZmZWVlYzVhOWEifQ.IR7ZRxLEFo4MWi6QIWGSUQ")
+            .success(
+            function (result) {
+                console.log(result);
+                if (result.features.length > 0) {
+                    $scope.parking.latitude = result.features[0].center[1];
+                    $scope.parking.longitude = result.features[0].center[0];
+                    $scope.markers.mainMarker.lat = $scope.parking.latitude;
+                    $scope.markers.mainMarker.lng = $scope.parking.longitude;
+                } else {
+                    alert("Aucun emplacement trouvé pour cette adresse.")
+                }
+            }
+        );
+    };
+
     $scope.$on("leafletDirectiveMarker.dragend", function (event, args) {
         $scope.parking.latitude = args.model.lat;
         $scope.parking.longitude = args.model.lng;
