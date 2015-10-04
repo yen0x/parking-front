519971f0c78d563ffb13c3235e4e64da84434159
remeh <remeh@remeh.fr>
2015-10-04 19:23:56 +0200

    parking: add a basic parkings list in the dashboard.

---
+++ app/components/parkinglist/parkinglist.js
@@ -0,0 +1,29 @@
+var app = angular.module('parking.controllers');
+
+app.controller('ParkingListController', ['$scope', '$http', 'Auth', function ($scope, $http, Auth) {
+  $scope.parkings = [];
+
+  $scope.listParkings = function() {
+      $http.get('/api/parking/list')
+          .then(function(response) {
+              if (response.status !== 200) {
+                // TODO(remy): deal with something else than a 200.
+              }
+              if (response.data.length === 0) {
+                // TODO(remy): deal with no results
+                return;
+              }
+
+              $scope.parkings = response.data;
+              console.log(response.data);
+          },
+          function(response) {
+              // TODO(remy): deal with an error.
+              console.log('error:');
+              console.log(response);
+          });
+  };
+
+  $scope.listParkings();
+}]);
+
